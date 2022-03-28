a78c1330da10b58a483d3f5ee0384bd8d4f1ed57
anaghirghilijiu <ana.ghirghilijiu.com>
2022-03-28 20:08:20 +0000

    changes

--- module/global_vpc.tf
+++ module/global_vpc.tf
@@ -1,10 +1,10 @@
-module "vpc" {
+module "global_vpc" {
   source  = "../"
     billing_acct  = "My Billing Account"
     project_name  = "testmodule"
     vpc_name      = "vpcg"
-    region        = "us-west1"
-    zone          = "us-west1-a"
+    region        = "us-west4"
+    zone          = "us-west4-a"
     firewall_name = "fw-allow-http"
     network_tags  = ["webserver"] 
 }
