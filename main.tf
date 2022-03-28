8711546bdb9833c88807321f3be4566e97fdb6a8
anaghirghilijiu <ana.ghirghilijiu.com>
2022-03-28 13:56:38 +0000

    no tags

--- main.tf
+++ main.tf
@@ -3,5 +3,4 @@ resource "google_compute_network" "vpcglobal" {
   name                    = var.vpc_name
   auto_create_subnetworks = "true"
   routing_mode            = "GLOBAL"
-  tags                    = var.labels
 }
