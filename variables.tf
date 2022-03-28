b49a1eec49fb2e80afb7ca95c865cc939b666f00
anaghirghilijiu <ana.ghirghilijiu.com>
2022-03-28 13:50:00 +0000

    testing

---
+++ variables.tf
@@ -0,0 +1,39 @@
+variable "billing_acct" {
+  description = "Please provide billing account name"
+  type        = string
+}
+
+variable "project_name" {
+  description = "Please provide project name"
+  type        = string
+}
+
+variable "vpc_name" {
+  description = "Please provide VPC name to bulid a VPC"
+  type        = string
+}
+
+variable "region" {
+  description = "Please provide a region to bulid a VPC"
+  type        = string
+}
+
+variable "zone" {
+  description = "Please provide a zone to bulid a VPC"
+  type        = string
+}
+
+variable "firewall_name" {
+  description = "Please provide a firewall name to bulid a VPC"
+  type        = string
+}
+
+variable "network_tags" {
+  description = "Please provide a network tag name to bulid a VPC"
+  type        = list(any)
+}
+
+variable "labels" {
+  description = "Please provide LABELS to bulid a VPC"
+  type        = map(any)
+}
